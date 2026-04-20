6b88c0659ed8f2ac397da11ee16276bc6161155d
Vladlen Kasimov <vladlenkas06home@gmail.com>
2026-04-20 21:21:34 +0500

    Update conf

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // base: '/Web-site-os-game-lz-2026/',
   plugins: [
     vue(),
     vueDevTools(),
