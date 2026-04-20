75894783983a88a25f1e2d3bf9e30a1c25f03066
Vladlen Kasimov <vladlenkas06home@gmail.com>
2026-04-20 20:58:38 +0500

    Fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/os-game-lz/',
   plugins: [
     vue(),
     vueDevTools(),
